b074bd67efe24c7859bca42160c95d1b85f6db99
Your Name <you@example.com>
2026-02-17 18:27:45 +1100

    inference script

--- src/megapose/infer.py
+++ src/megapose/infer.py
@@ -8,6 +8,8 @@ import numpy as np
 from megapose.inference.utils import make_detections_from_object_data
 from megapose.utils.load_model import NAMED_MODELS, load_named_model
 from dataloader import YCBV_LF
+from tqdm import tqdm
+import os
 
 
 def bounding_box_from_mask(binary_mask: np.ndarray):
@@ -39,45 +41,40 @@ def make_object_dataset(example_dir):
     return rigid_object_dataset
 
 
-if __name__ == "__main__":
-
-    dataset = YCBV_LF(
-        "/home/ngoncharov/cvpr2026/megapose6d/datasets/ycbv_lf/bleach_hard_00_03_chaitanya"
-    )
-    sample = dataset[0]
-    rgb = sample["rgb"]
-    depth = sample["depth"]
-    bounding_box = bounding_box_from_mask(sample["mask"])
-    # from matplotlib import pyplot as plt
-
-    # plt.imshow(rgb)
-    # plt.scatter([bounding_box[0], bounding_box[2]], [bounding_box[1], bounding_box[3]], color="red")
-    # plt.savefig("test.png")
-    # raise
-    observation = ObservationTensor.from_numpy(rgb, depth, dataset.camera_intrinsics).cuda()
-    detections = make_detections_from_object_data(
-        [ObjectData(label=dataset.model_name, bbox_modal=bounding_box)]
-    ).cuda()
-
+def get_poses(dataset):
+    poses = []
     model_name = "megapose-1.0-RGBD"
     model_info = NAMED_MODELS[model_name]
-
     rigid_object_dataset = RigidObjectDataset(
         [RigidObject(label=dataset.model_name, mesh_path=dataset.model_path, mesh_units="mm")]
     )
     pose_estimator = load_named_model(model_name, rigid_object_dataset).cuda()
-    # print(observation)
-    # print(detections)
-    # print(model_info["inference_parameters"])
-    # raise
-    output, _ = pose_estimator.run_inference_pipeline(
-        observation,
-        detections=detections,
-        **model_info["inference_parameters"],
-        cuda_timer=True,
-    )
-    poses = output.poses.cpu().numpy()
-    print(poses)
+    for sample in tqdm(dataset):
+        rgb = sample["rgb"]
+        depth = sample["depth"]
+        bounding_box = bounding_box_from_mask(sample["mask"])
+        observation = ObservationTensor.from_numpy(rgb, depth, dataset.camera_intrinsics).cuda()
+        detections = make_detections_from_object_data(
+            [ObjectData(label=dataset.model_name, bbox_modal=bounding_box)]
+        ).cuda()
+        output, _ = pose_estimator.run_inference_pipeline(
+            observation,
+            detections=detections,
+            **model_info["inference_parameters"],
+            cuda_timer=True,
+        )
+        pose = output.poses.cpu().numpy()[0]
+        poses.append(pose)
 
-    # for key, value in sample.items():
-    #     print(key)
+    return np.stack(poses)
+
+
+if __name__ == "__main__":
+    data_path = "/home/ngoncharov/cvpr2026/megapose6d/datasets/ycbv_lf/bleach_hard_00_03_chaitanya"
+    dataset = YCBV_LF(data_path)
+    results_path = "results_megapose6d/" + data_path.split("/")[-2]
+    print(results_path)
+    os.makedirs(results_path, exist_ok=True)
+    poses = get_poses(dataset)
+    print(poses)
+    np.save(f"{results_path}/{data_path.split('/')[-1]}.npy", poses)
